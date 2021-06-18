f1674b393c09d1e2be52ba687c6759e3f446863b
christopher <angel.rmarin@outlook.com>
2021-06-18 14:10:12 -0500

    added subheader}

--- main.js
+++ main.js
@@ -56,10 +56,6 @@ function createTable(labels, container, data) {
     thead.appendChild(theadTr);
     table.appendChild(thead);
 
-
-
-    
-
     for (let j = 0; j < data.length; j++) {
         let tbodyTr = document.createElement('tr');
         for (let k = 0; k < labels.length; k++) {
