b8259024e0731f911dcfcef591f00fa93a7cbe2a
christopher <angel.rmarin@outlook.com>
2021-06-18 14:03:18 -0500

    test richard

--- main.js
+++ main.js
@@ -56,6 +56,10 @@ function createTable(labels, container, data) {
     thead.appendChild(theadTr);
     table.appendChild(thead);
 
+
+
+    
+
     for (let j = 0; j < data.length; j++) {
         let tbodyTr = document.createElement('tr');
         for (let k = 0; k < labels.length; k++) {
@@ -71,3 +75,5 @@ function createTable(labels, container, data) {
 
 }
 
+
+Random(120,123)
